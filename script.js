7c96e833c3d284bce7c2c016a76054eb9fdc90d6
Jason Hensley <jasonhensley1989@gmail.com>
2023-01-31 14:23:49 -0600

    basic recursive function implemented alongside non recursive basic for loop

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let countdown = (num) => {
+let countDown = (num) => {
     for(let i = num; i > 0; i--) {
         console.log(i);
     }
     console.log("Countdown is ended");
+}
+
+// this loop is 
+let countDownRecursive = (num) => {
+    if(num <= 0) {
+        console.log('Countdown has ended recursively');
+        return
+    }
+    console.log(num)
+
+    // code is num - 1 because its performing the loop that subtracts one each round. 
+    countDownRecursive(num - 1);
 }
