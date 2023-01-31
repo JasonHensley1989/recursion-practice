e51b0f108f85fe75f4481c051bda2bc8be131f1b
Jason Hensley <jasonhensley1989@gmail.com>
2023-01-31 14:41:10 -0600

    recursive function to determine sum of range

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ let countDownRecursive = (num) => {
     // code is num - 1 because its performing the loop that subtracts one each round. 
     countDownRecursive(num - 1);
 }
+
+// the process occuring here is an up and down search
+            // countDownRecursive(3);
+            //     countDownRecursive(2);
+            //         countDownRecursive(1);
+            //             countDownRecursive(0);
+            //             return
+            //         return
+            //     return
+            // return
+
+
+
+// here we will make a more practical example where are taking an input and constantly growing our 
+// input inside of our recursive functions
+
+// this is the initial example to be refactored
+let sumOfRange = (n) => {
+    let total = 0;
+    for(let i = n; i > 0; i--) {
+        total += i
+    }
+    return total;
+}
+
